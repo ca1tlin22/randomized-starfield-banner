4e091403b55778134da27c5b82090699937bfe13
ca1tlin22 <gagn0372@algonquinlive.com>
2016-04-05 10:43:19 -0400

    Variables defined

---
+++ js/main.js
@@ -0,0 +1,9 @@
+var $banner = $('.banner');
+var $addStars;
+var $width = $banner.outerWidth();
+var $height = $banner.outerHeight();
+
+var $body = $('.banner');
+var $star;
+var width = $body.outerWidth();
+var height = $body.outerHeight();
